fc7c3b03bf7fdcbf3021a709bbeecb949a806a90
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 11:43:27 +0700

    Add styles

--- todos/src/Components/ToDoElem.jsx
+++ todos/src/Components/ToDoElem.jsx
@@ -4,9 +4,9 @@ import '../Styles/ToDoElem.css'
 export default function ToDoElem({todo}){
     return (
         <div className='ToDo'>
-            <h1>{todo.id}</h1>
-            <h1>{todo.name}</h1>
-            <h1>{todo.status}</h1>
+            <h1 className='id'>{todo.id}</h1>
+            <h1 className='name'>{todo.name}</h1>
+            <h1 className='status'>{todo.status}</h1>
         </div>
     );
 }
