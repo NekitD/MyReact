1ecff7ccde3dd798b6cd074e2645c5ccc53d4b07
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 20:36:19 +0700

    Add empty form

--- sibsutis_uno/src/components/TeacherForm.jsx
+++ sibsutis_uno/src/components/TeacherForm.jsx
@@ -2,24 +2,35 @@ import React from 'react'
 import '../styles/TeacherForm.css'
 
 export default function TeacherForm({data}){
-    return (
-        <div className='Teachers__Form'>
-            <div className='Teachers__Header'>
-                <img className='Teachers__Img' src={data?.image}/>
-                <div className='Teachers__Names'>
-                    <h2>Полное имя:</h2>
-                    <h1 className='Teachers__Name'>{data?.name}</h1>
-                    <h2>Также известен как:</h2>
-                    <h2 className='Teachers__Nick'>{data?.nickname}</h2>
+
+    if(data){
+        return (
+            <div className='Teachers__Form'>
+                <div className='Teachers__Header'>
+                    <img className='Teachers__Img' src={data?.image}/>
+                    <div className='Teachers__Names'>
+                        <h2>Полное имя:</h2>
+                        <h1 className='Teachers__Name'>{(data.name) ? data.name : "..."}</h1>
+                        <h2>A.k.a:</h2>
+                        <h2 className='Teachers__Nick'>{(data.nickname) ? data.nickname : "..."}</h2>
+                    </div>
+                </div>
+                <h1>Описание:</h1>
+                <div className='Teachers__Description'>
+                    {data?.description}
+                </div>
+                <div className='Teachers__Comments'>
+                    {data?.comments}
                 </div>
             </div>
-            <h1>Описание:</h1>
-            <div className='Teachers__Description'>
-                {data?.description}
+        );
+    } else {
+        return (
+            <div className='Teachers__Form___Empty'>
+                <h1 className='Teachers__EmptyMessage'>
+                    Выберите препода...
+                </h1>
             </div>
-            <div className='Teachers__Comments'>
-                {data?.comments}
-            </div>
-        </div>
-    );
+        );
+    }
 }
