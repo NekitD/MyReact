b2ae9c5ba2c86398199677e541541892e053bb7b
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 11:10:30 +0700

    Delete  index style

--- todos/src/Components/ToDoList.jsx
+++ todos/src/Components/ToDoList.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import ToDo from './Components/ToDoElem'
+import ToDo from './ToDoElem'
 
 export default function ToDoList({list}){
     return (
         <div className='ToDoList'>
-            {list.map(
-                (elem)=>{<ToDo key={elem.id} todo={elem}/>}
-            )}
+            {list.map((elem)=>{<ToDo key={elem.id} todo={elem}/>})}
         </div>
     );
 }
