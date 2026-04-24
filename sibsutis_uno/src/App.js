eceef91964e27931579db68d9169ad449d906565
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 17:40:45 +0700

    Add navbar hover style

--- sibsutis_uno/src/App.js
+++ sibsutis_uno/src/App.js
@@ -3,10 +3,11 @@ import Preview from './components/Preview'
 import Navbar from './components/Navbar'
 
 export default function App() {
+  const tabs = [{name: "Предметы"}, {name: "Преподы"}]
   return (
     <div className="App">
       <Preview/>
-      <Navbar/>
+      <Navbar tabs={tabs}/>
     </div>
   );
 }
--- sibsutis_uno/src/components/Navbar.jsx
+++ sibsutis_uno/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/Navbar.css'
+import Tab from "./NavButton"
 
-export default function Navbar(){
+export default function Navbar({tabs}){
     return (
         <div className='Navbar'>
-
+            {tabs.map((tab)=>{return <Tab name={tab.name}/>})
+            }
         </div>
     );
 }
