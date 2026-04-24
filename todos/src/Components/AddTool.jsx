528bd1a59d62deb83facce6424daafe67a2262ba
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 13:39:50 +0700

    Apply Add tool style

--- todos/src/Components/AddTool.jsx
+++ todos/src/Components/AddTool.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import '../Styles/AddTool.css'
 
 export default function AddTool({addCallback}){
     const [name, setName] = useState('');
     return (
         <form className='AddTool'>
-            <input value={name} className="AddTool__field" type="text" id="name" placeholder='Название' onChange={(e)=>{setName(e.target.value)}} required/>
+            <input className="AddTool__field" type="text" value={name} placeholder='Название' onChange={(e)=>{setName(e.target.value)}} required/>
             <button onClick={(event)=>{
                         event.preventDefault(); 
                         addCallback({name: name, status: "Получено"});
