91fc69af2a076a68fd8a17b882c3d68fd372279d
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 11:12:52 +0700

    Fix list

--- todos/src/Components/ToDoList.jsx
+++ todos/src/Components/ToDoList.jsx
@@ -4,7 +4,9 @@ import ToDo from './ToDoElem'
 export default function ToDoList({list}){
     return (
         <div className='ToDoList'>
-            {list.map((elem)=>{<ToDo key={elem.id} todo={elem}/>})}
+            {list.map((elem)=>{
+                return <ToDo key={elem.id} todo={elem}/>
+            })}
         </div>
     );
 }
