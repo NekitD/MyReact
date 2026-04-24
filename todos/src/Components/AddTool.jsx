9b71feb5cbf4c71a942e88a4822d4a2069ea4f6d
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 13:07:32 +0700

    Add clearing field

--- todos/src/Components/AddTool.jsx
+++ todos/src/Components/AddTool.jsx
@@ -4,7 +4,7 @@ export default function AddTool({addCallback}){
     const [name, setName] = useState('');
     return (
         <form className='AddTool'>
-            <input className="AddTool__field" type="text" id="name" placeholder='Название' onChange={(e)=>{setName(e.target.value)}} required/>
+            <input value={name} className="AddTool__field" type="text" id="name" placeholder='Название' onChange={(e)=>{setName(e.target.value)}} required/>
             <button onClick={(event)=>{
                         event.preventDefault(); 
                         addCallback({name: name, status: "Получено"});
