219c8b5efcbc0ee527c179c091fa37ab744f526a
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-24 17:16:13 +0700

    Add mai navbar body

--- sibsutis_uno/src/App.js
+++ sibsutis_uno/src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import Preview from './components/Preview'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div className="App">
       <Preview/>
+      <Navbar/>
     </div>
   );
 }
