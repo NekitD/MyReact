2116ee7cf8bce6d29b6daf5174f20711e5f17bf4
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-25 13:23:26 +0700

    Add filters to searcher

--- sibsutis_uno/src/components/Searcher.jsx
+++ sibsutis_uno/src/components/Searcher.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Searcher.css'
 
 export default function Searcher({filters}){
+    const [activeFilters, setActiveFilters] = useState(false);
     return (
         <div className='Searcher'>
             <input className='Searcher__String' type='text' placeholder='Поиск'/>
-            <button className='Searcher__FilterOpener'/>
-            <div className='Searcher__Filters'>
-
-            </div>
+            <button className='Searcher__FilterOpener' onClick={()=>{setActiveFilters(activeFilters === false)}}/>
+            {
+                (activeFilters) ?
+                <div className='Searcher__Filters'>
+                    {
+                        filters.map((filter)=>{
+                            return (
+                                <div className='Searcher__Filter'>
+                                    <h4>{filter.name}</h4>
+                                    {filter.values.map((value)=>{
+                                        return (
+                                            <button className='Searcher__FilterValue'>
+                                                {value}
+                                            </button>
+                                        );
+                                    })}
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+                : null
+            }
         </div>
     );
 }
