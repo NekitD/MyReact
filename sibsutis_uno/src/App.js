ff3d4eedff84bcacbc97751ecce9534d8f3ff8fd
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-25 12:33:11 +0700

    Import JSON data to App

--- sibsutis_uno/src/App.js
+++ sibsutis_uno/src/App.js
@@ -3,24 +3,14 @@ import Preview from './components/Preview'
 import Navbar from './components/Navbar'
 import { useState } from 'react';
 import Teachers from './components/Teachers'
+import TeachersData from "./data/teachers.json"
+import TabsData from "./data/tabs.json"
 
 export default function App() {
 
   const [selectedTab, setSelectedTab] = useState(0);
-  const tabs = [{id: 1, name: "Предметы"}, {id: 2, name: "Преподы"}]
-  const teachers = [
-    {id: 1, name: "Трофимов Виктор Куприянович", 
-      nickname: " Куприяныч",
-      description: "Не уметь решать дифференциальные уравнения второго порядка с определёнными коэффициентами и специальной правой частью - это зашквар! Вы что, не умеете решать квадратные уравнения?",
-      image: "/img/Trofimov.jpg"
-    }, 
-    {id: 2, name: "Милешко Антон Владимирович"}, 
-    {id: 3, name: "Чудинов Сергей Клэшроялевич"},
-    {id: 4, name: "Агалаков Антон Александрович"},
-    {id: 5, name: "Дементьева Кристина Игоревна"},  
-    {id: 6, name: "Ситняковская Елена Игоревна"},
-    {id: 7, name: "Орлов Илья Олегович"},
-  ]
+  const tabs = TabsData;
+  const teachers = TeachersData;
   return (
     <div className="App">
       <div className='App__header'>
