7d30830cc828de10cb2d1c0ada5668c89e69801f
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-27 14:31:09 +0700

    Add subjects prop

--- sibsutis_uno/src/App.js
+++ sibsutis_uno/src/App.js
@@ -6,12 +6,16 @@ import Teachers from './components/Teachers'
 import TeachersData from "./data/teachers.json"
 import TabsData from "./data/tabs.json"
 import TeacherFilters from "./data/teachers_filters.json"
+import Subjects from "./components/Subjects"
+import SubjectsData from "./data/subjects.json"
+import SubjectsFilters from "./data/subjects_filters.json"
 
 export default function App() {
 
   const [selectedTab, setSelectedTab] = useState(0);
   const tabs = TabsData;
   const teachers = TeachersData;
+  const subjects = SubjectsData;
   return (
     <div className="App">
       <div className='App__header'>
@@ -19,10 +23,10 @@ export default function App() {
         <Navbar tabs={tabs} callback={setSelectedTab} selected={selectedTab}/>
       </div>
       <div className='App__body'>
+        {(selectedTab === 1) ? <Subjects data={subjects} filters={SubjectsFilters}/> : null}
         {(selectedTab === 2) ? <Teachers data={teachers} filters={TeacherFilters}/> : null}
       </div>
       <div className='App__footer'>
-
       </div>
     </div>
   );
---
+++ sibsutis_uno/src/components/Subjects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Subjects(){
+    return (
+        <div className='Subjects'>
+
+        </div>
+    );
+}
